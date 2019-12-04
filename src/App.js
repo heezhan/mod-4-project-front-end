4e3f52b798f63e3002b027f6d22ac8f69f6b7f54
heezhan <hhan1194@gmail.com>
2019-12-04 09:24:37 -0500

    added home path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import FiltersContainer from './containers/FiltersContainer';
 import ExercisesContainer from './containers/ExercisesContainer';
+import {Route} from "react-router-dom";
 
 class App extends React.Component {
 
@@ -23,8 +24,6 @@ class App extends React.Component {
       shownExercises: exerciseArray
     })
     )
-
-
   }
 
   handleMuscleGroupChange = (event) => {
@@ -41,22 +40,18 @@ class App extends React.Component {
       shownExercises: filteredExercises
     })
   }
-
-
-
-
-
   
-
   render() {
     return (
       <div className="App">
         <NavBar />
+        <Route exact path="/">
         <FiltersContainer 
           selectedMuscleGroup={this.state.selectedMuscleGroup}
           handleMuscleGroupChange={this.handleMuscleGroupChange}
         />
         <ExercisesContainer shownExercises={this.state.shownExercises}/>
+        </Route>
       </div>
     );
   }
